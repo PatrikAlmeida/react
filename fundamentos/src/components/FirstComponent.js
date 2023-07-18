55b2d01fcdd0920d1e5ed0dc74bfd549aa412098
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-18 16:28:43 -0300

    comentarios

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,8 +1,17 @@
+// arquivo de estilo
+
 const FirstComponent = () => {
+    // essa função faz isso
+
+    /*
+        multi line
+    */
 
     return (
         <div>
+            {/* Algum comentário */}
             <h1>Meu primeiro componente</h1>
+            <p className="teste">Meu texto</p>
         </div>
     )
 }
