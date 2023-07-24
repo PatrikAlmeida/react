91f9e4ed193048fe9515b72aa19091f9bc2516ca
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-24 11:57:20 -0300

    destructuring em props

--- avancando/src/App.js
+++ avancando/src/App.js
@@ -6,6 +6,7 @@ import ConditionalRender from './components/ConditionalRender';
 import ListRender from './components/ListRender';
 import ManageData from './components/ManageData';
 import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
 
 function App() {
   const name = "Joaquim"
@@ -25,7 +26,10 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/* props */}
       <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul" />
     </div>
   );
 }
