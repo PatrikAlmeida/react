d018e3657489b9bbcc1148a9ec62770fd4faed1a
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-26 15:03:06 -0300

    desafio sobre componentes e recursos do react

--- avancando/src/App.js
+++ avancando/src/App.js
@@ -12,7 +12,7 @@ import Container from './components/Container';
 import ExecuteFunction from './components/ExecuteFunction';
 import Message from './components/Message';
 import ChangeMessageState from './components/ChangeMessageState';
-
+import UserDetails from './components/UserDetails';
 
 
 function App() {
@@ -34,6 +34,13 @@ function App() {
   const handleMessage = (msg) => {
     setMessage(msg)
   }
+
+  const users = [
+    { id: 1, name: "Matheus", job: "Programador", age: 31 },
+    { id: 2, name: "Maria", job: "Advogada", age: 40 },
+    { id: 3, name: "Josias", job: "Repórter", age: 66 },
+    { id: 4, name: "Miguel", job: "Estudante", age: 16 },
+  ]
 
   return (
     <div className="App">
@@ -80,6 +87,15 @@ function App() {
       {/* state lift */}
       <Message msg={message} />
       <ChangeMessageState handleMessage={handleMessage} />
+      {/* desafio */}
+      {users.map((user) => (
+        <UserDetails
+          key={user.id}
+          name={user.name}
+          job={user.job}
+          age={user.age}
+        />
+      ))}
     </div>
   );
 }
