53865510bebf8e81f869ff0f5487e689319c941e
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-31 18:02:15 -0300

    envio de form

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -11,14 +11,21 @@ const MyForm = () => {
         setName(e.target.value)
     }
 
-    console.log(name)
-    console.log(email)
+    // console.log(name)
+    // console.log(email)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("Enviando o formulário")
+        console.log(name, email)
+    }
 
 
   return (
     <div>
+        {/* 5- envio de form */}
         {/* 1= criação de form */}
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
@@ -26,6 +33,7 @@ const MyForm = () => {
         {/* 2- label envolvendo input */}
             <label>
                 <span>E-mail</span>
+                {/* 4- simplificação de manipulação de state */}
                 <input
                  type="email" 
                  name="email" 
