aa61f22264bc1d119f417c5b6dc42286cd8bf902
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-31 18:28:01 -0300

    textarea no react

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -1,59 +1,77 @@
-import './MyForm.css'
+import "./MyForm.css";
 
-import {useState} from 'react'
+import { useState } from "react";
 
 const MyForm = ({ user }) => {
-    // 6- controlled inputs
-    // 3- gerenciamento de dados
-    const [name, setName] = useState(user ? user.name : "")
-    const [email, setEmail] = useState(user ? user.email : "")
+  // 6- controlled inputs
+  // 3- gerenciamento de dados
+  const [name, setName] = useState(user ? user.name : "");
+  const [email, setEmail] = useState(user ? user.email : "");
 
-    const handleName = (e) => {
-        setName(e.target.value)
-    }
+  const [bio, setBio] = useState("");
 
-    // console.log(name)
-    // console.log(email)
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log("Enviando o formulário")
-        console.log(name, email)
+  // console.log(name)
+  // console.log(email)
 
-        // validação
-        // envio
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando o formulário");
+    console.log(name, email, bio);
 
-        // 7- limpar form
-        setName("")
-        setEmail("")
-    }
+    // validação
+    // envio
 
+    // 7- limpar form
+    setName("");
+    setEmail("");
+    setBio("")
+  };
 
   return (
     <div>
-        {/* 5- envio de form */}
-        {/* 1= criação de form */}
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} value={name} />
-            </div>
+      {/* 5- envio de form */}
+      {/* 1= criação de form */}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Nome:</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+            value={name}
+          />
+        </div>
         {/* 2- label envolvendo input */}
-            <label>
-                <span>E-mail</span>
-                {/* 4- simplificação de manipulação de state */}
-                <input
-                 type="email" 
-                 name="email" 
-                 placeholder="Digite o seu e-mail" 
-                 onChange={(e) => setEmail(e.target.value)}
-                 value={email} 
-                 />
-            </label>
-            <input type="submit" value="Enviar" />
-        </form>
+        <label>
+          <span>E-mail</span>
+          {/* 4- simplificação de manipulação de state */}
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        {/* 8- textarea */}
+        <label>
+          <span>Bio:</span>
+          <textarea
+            name="bio"
+            placeholder="Descrição do usuário"
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+          ></textarea>
+        </label>
+        <input type="submit" value="Enviar" />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
