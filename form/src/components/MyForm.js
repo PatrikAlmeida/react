e80044d2b0f61c82e1735d31e132f334179af04f
Patrik de Almeida <patrikalmeida1@hotmail.com>
2023-07-31 16:35:44 -0300

    alteração de state inline

--- form/src/components/MyForm.js
+++ form/src/components/MyForm.js
@@ -12,6 +12,7 @@ const MyForm = () => {
     }
 
     console.log(name)
+    console.log(email)
 
 
   return (
@@ -25,7 +26,12 @@ const MyForm = () => {
         {/* 2- label envolvendo input */}
             <label>
                 <span>E-mail</span>
-                <input type="email" name="email" placeholder="Digite o seu e-mail" />
+                <input
+                 type="email" 
+                 name="email" 
+                 placeholder="Digite o seu e-mail" 
+                 onChange={(e) => setEmail(e.target.value)} 
+                 />
             </label>
             <input type="submit" value="Enviar" />
         </form>
